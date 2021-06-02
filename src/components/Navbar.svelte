93edbddaac33687806dd7ce4cd0ba29e9136710c
Omar Yakoubi <yakoubiomar2001@gmail.com>
2021-06-02 11:52:09 +0100

    navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,18 @@
+<nav class="navbar fixed-top navbar-dark bg-dark">
+  <div class="container-fluid">
+    <h3 id="logo">TODO <span>LIST</span></h3>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Add todo" />
+      <button class="btn btn-outline-primary" type="submit">Add</button>
+    </form>
+  </div>
+</nav>
+
+<style>
+  #logo {
+    color: blue;
+  }
+  span {
+    color: white;
+  }
+</style>
